07ac459de1f928c5d9e01aaf8b090c7e242fe04c
palewire <ben.welsh@latimes.com>
2009-09-05 16:28:48 -0700

    Improved managers on the UpdateLog so I can get some more useful data

--- update/models.py
+++ update/models.py
@@ -9,11 +9,37 @@ class LiveUpdateManager(models.Manager):
 	
 	Example usage:
 		
-		>>> Update.objects.live()
+		>>> UpdateLog.objects.complete()
+		>>> UpdateLog.objects.updates()
+		>>> UpdateLog.objects.last_scraped()
+		>>> UpdateLog.objects.last_updated()
 		
 	"""
-	def live(self):
+	def complete(self):
 		return self.filter(outcome='complete')
+
+	def updates(self):
+		return self.filter(outcome='complete', loaded_new_data=True)
+
+	def last_scraped(self):
+		"""
+		Return the date of the last the source was scraped.
+		"""
+		try:
+			latest_obj = self.complete().latest()
+			return latest_obj.end_date
+		except UpdateLog.DoesNotExist:
+			return None
+
+	def last_updated(self):
+		"""
+		Returns the date of the last time the data were modified.
+		"""
+		try:
+			latest_obj = self.updates().latest()
+			return latest_obj.end_date
+		except UpdateLog.DoesNotExist:
+			return None
 
 
 class UpdateLog(models.Model):
@@ -42,5 +68,6 @@ class UpdateLog(models.Model):
 	
 	class Meta:
 		db_table = 'rapture_update_log'
-		get_latest_by = 'update_end_date'
+		get_latest_by = 'end_date'
 		verbose_name_plural = _('update log')
+		
