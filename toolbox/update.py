6df65c878bb0bd750af9c9a057c4be4714274d87
palewire <ben.welsh@latimes.com>
2009-09-05 13:54:03 -0700

    Expanded scrape so that it will retrieve images and css as well as html

--- toolbox/update.py
+++ toolbox/update.py
@@ -63,6 +63,36 @@ def pull():
 	outfile_path = os.path.join(html_dir, 'rap2.html')
 	outfile = open(outfile_path, 'w')
 	print >> outfile, soup.prettify()
+	
+	# Create a list of all the resources the page calls
+	# that we want to download so we can recreate it later.
+	resources = []
+	
+	# Images
+	for img in soup.findAll("img", {"src":True}):
+		resources.append(img["src"])
+		
+	# Cascading Style Sheets
+	for link in soup.findAll("link", {"rel":"stylesheet", "type":"text/css"}):
+		resources.append(link["href"])
+
+	# Alternative Cascading Style Sheets
+	for link in soup.findAll("link", {"rel":"alternate stylesheet", "type":"text/css"}):
+		resources.append(link["href"])
+		
+	# Download all the images to the archive.
+	for r in resources:
+		# Split the relative path to the img
+		head, tail = os.path.split(r)
+
+		# Create a directory to store the file
+		r_dir = os.path.join(html_dir, head)
+		_mkdir(r_dir)
+		
+		# Path together an absolute URL we can download
+		url = 'http://www.raptureready.com' + r
+		local_path = os.path.join(r_dir, tail)
+		urllib.urlretrieve(url, local_path)
 
 	# Narrow down to the table containing the rankings
 	table = soup.find('table', attrs={
@@ -137,5 +167,5 @@ def load(data_dict, timestamp):
 if __name__ == '__main__':
 	data_dict, timestamp = pull()
 	from pprint import pprint
-	pprint(data_dict)
+	#pprint(data_dict)
 	#load(data_dict, timestamp)
